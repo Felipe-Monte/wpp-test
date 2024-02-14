b2903633682b277a504d1b1b6cbd1fe434db9e6e
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-02-14 19:17:43 -0300

    test wpp send

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
 function sendMessage(){
-  const inputNumber = document.querySelector("input").value
+  const inputNumber = document.querySelector("#number").value
+  const name = document.querySelector("#name").value
+
+  if(inputNumber === "" || name === "") {
+    alert("Campo vazio preencha os dados!")
+    return
+  }
   
-  const message = "Isso é uma menssagem automática"
+  const message = `Olá ${name} Isso é uma menssagem automática criada para cobrar contas em atraso !`
 
   var linkWhatsApp = "https://wa.me/" + `+5584${inputNumber}` + "?text=" + encodeURIComponent(message);
     
